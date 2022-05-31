fdd7d4ae436e30ca8c2c8dcb164ee15b9a33b865
Steven Calapaqui <scalapaquir@est.ups.edu.ec>
2022-05-31 10:34:52 -0500

    Creada la clase Persona

--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
-
+#include"persona.h"
 using namespace std;
 
 int main()
 {
-    cout << " Hola" << endl;
+    cout << " Mi primer proyecto en Git" << endl;
+    Persona p;
+    p.nombre= "Steven";
+    p.edad = 20;
+    cout<< p.nombre <<endl;
+    cout<< p.edad<< endl;
     return 0;
 }
---
+++ persona.h
@@ -0,0 +1,15 @@
+#ifndef PERSONA_H
+#define PERSONA_H
+
+#include<string>
+using namespace std;
+class Persona
+{
+public:
+    string nombre;
+    int edad;
+    Persona();
+
+};
+
+#endif // PERSONA_H
